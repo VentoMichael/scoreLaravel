6b3b21ea7df15fc05fd8902041710c34127418a9
VentoMichael <vento.michael0705@hotmail.com>
2020-09-24 13:55:54 +0200

    add migrations

---
+++ database/migrations/2020_09_24_111658_create_matches_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMatchesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('matches', function (Blueprint $table) {
+            $table->id();
+            $table->string('slug');
+            $table->timestamps();
+
+            $table->foreign('post_id')->references('match_id')->on('participations')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('matches');
+    }
+}
---
+++ database/migrations/2020_09_24_112850_create_participations_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateParticipationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('participations', function (Blueprint $table) {
+            $table->id();
+            $table->string('match_id');
+            $table->string('team_id');
+            $table->tinyInteger('goals');
+            $table->tinyInteger('is_home');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('participations');
+    }
+}
